829be0bae181618a140aa6d64bb82a9850cea7f3
AlexSh1mada <alexbuerom@gmail.com>
2024-06-24 08:11:44 -0600

    Improved volume slider 🖌️🟢

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -18,7 +18,7 @@ const Slider = React.forwardRef<
     {...props}
   >
     <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-[#4D4D4D]">
-    <SliderPrimitive.Range className="absolute h-1 bg-white cursor-pointer group-hover:bg-green-500 peer-focus:bg-green-500" />
+    <SliderPrimitive.Range className="absolute h-1 bg-white group-hover:bg-green-500 peer-focus:bg-green-500" />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb className="block opacity-0 group-hover:opacity-100 size-3 rounded-full cursor-pointer bg-white transition-colors focus-visible:outline-none focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
