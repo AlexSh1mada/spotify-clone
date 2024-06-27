5511922ac66543cbea370e1989eb92fa36b4bb12
AlexSh1mada <alexbuerom@gmail.com>
2024-06-27 01:59:49 -0600

    audio slider fix

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -29,12 +29,20 @@ export const Play = ({ className }) => (
 );
 
 export const VolumeSilence = () => (
-	<svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volumen apagado" viewBox="0 0 16 16" ><path d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06z"></path><path d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.642 3.642 0 0 0-1.33 4.967 3.639 3.639 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.73 4.73 0 0 1-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path></svg>
+	<svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volume off" viewBox="0 0 16 16" ><path d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06z"></path><path d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.642 3.642 0 0 0-1.33 4.967 3.639 3.639 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.73 4.73 0 0 1-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path></svg>
   ) 
   
-  export const Volume = () => (
-	<svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volumen alto" id="volume-icon" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z"></path></svg>
+export const Volume = () => (
+	<svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volume hight" id="volume-icon" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z"></path></svg>
 	)
+
+export const VolumeMedium = () => (
+	<svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volume medium" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 6.087a4.502 4.502 0 0 0 0-8.474v1.65a2.999 2.999 0 0 1 0 5.175v1.649z"></path></svg>
+)
+
+export const VolumeLow = () => (
+	<svg fill="currentColor" role="presentation" height="16" width="16" aria-label="Volume low" aria-hidden="true" id="volume-icon" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path></svg>
+)
 
 const CurrentSong = ({ image, title, artists }) => {
 	return (
@@ -54,22 +62,72 @@ const CurrentSong = ({ image, title, artists }) => {
                 </h4>
 			</div>
 		</div>
-	);
-};
+	)
+}
+
+const SongControl = ({ audio }) => {
+	const [currentTime, setCurrentTime] = useState(0)
+
+	useEffect(() => {
+		audio.current.addEventListener('timeupdate', handleTimeUpdate)
+		return () => {
+			audio.current.removeEventListener('timeupdate', handleTimeUpdate)
+		}
+	})
+
+	const handleTimeUpdate = () => {
+		setCurrentTime(audio.current.currentTime)
+	}
+
+	const duration = audio?.current?.duration ?? 0
+
+	return (
+		<div className="flex gap-x-2">
+			<span>{currentTime}</span>
+				<Slider
+						defaultValue={[0]}
+						value={[currentTime]}
+						max={audio?.current?.duration ?? 0}
+						min={0}
+						className="w-[510px]"
+						step={1}
+						onValueChange={(value) => {
+							audio.current.currentTime = value
+						setVolume(volumeValue)
+					}}   
+				/>
+			<span>{duration}</span>
+		</div>
+	)
+}
 
 const VolumeControl = () => {
 	const volume = usePlayerStore(state => state.volume)
 	const setVolume = usePlayerStore(state => state.setVolume)
 	const previousVolumeRef = useRef(volume)
 
+	const isVolumeSilenced = volume === 0
+
+	const handleClickVolume = () => {
+		if(isVolumeSilenced) {
+			setVolume(previousVolumeRef.current)
+		} else {
+			previousVolumeRef.current = volume;;
+			setVolume(0)
+		}
+	}
+
 	return (
 		<div className="flex justify-center gap-x-2">
-		{ volume === 0 ? <VolumeSilence /> : <Volume /> }
+		<button className="opacity-70 hover:opacity-100 transition" onClick={handleClickVolume}>
+			{ isVolumeSilenced ? <VolumeSilence /> : volume < 0.3 ? <VolumeLow /> : volume < 0.65 ? <VolumeMedium /> : <Volume /> }
+		</button>
 		<Slider
 			defaultValue={[100]}
 			max={100}
 			min={0}
-			className="w-[100px]"
+			className="w-[95px]"
+			value={[volume * 100]}
 			step={1}
 			onValueChange={(value) => {
 			const [newVolume] = value
@@ -120,10 +178,11 @@ export function Player() {
 			</div>
 
 			<div className="grid place-content-center gap-4 flex-1">
-				<div className="flex justify-center">
+				<div className="flex justify-center flex-col items-center">
 					<button className="bg-white rounded-full p-2" onClick={handleClick}>
 						{isPlaying ? <Pause /> : <Play />}
 					</button>
+					<SongControl audio={audioRef} />
 				</div>
 			</div>
 
